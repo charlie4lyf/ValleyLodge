fa9f3b04c30a005e46d69386ac9de1550bdf470e
charlie4lyf <monzoracharles55@gmail.com>
2025-03-19 23:56:20 +0200

    second commit

--- valley/signals.py
+++ valley/signals.py
@@ -1,16 +1,19 @@
 from django.contrib.auth import get_user_model
 from django.db.models.signals import post_migrate
 from django.dispatch import receiver
-
+import os
 User = get_user_model()
 
 @receiver(post_migrate)
 def create_superuser(sender, **kwargs):
-    if sender.name == 'auth':  # Ensure it runs only for the auth app
-        SUPERUSER_USERNAME = "admin"
-        SUPERUSER_EMAIL = "monzorabrian@gmail.com"
-        SUPERUSER_PASSWORD = "admin123"
+    if sender.name == 'auth':  # Use your app name if using a custom user model
+        User = get_user_model()
+        username = os.getenv('DJANGO_SUPERUSER_USERNAME', 'admin')
+        email = os.getenv('DJANGO_SUPERUSER_EMAIL', 'admin@example.com')
+        password = os.getenv('DJANGO_SUPERUSER_PASSWORD', 'admin123')
 
-        if not User.objects.filter(username=SUPERUSER_USERNAME).exists():
-            User.objects.create_superuser(SUPERUSER_USERNAME, SUPERUSER_EMAIL, SUPERUSER_PASSWORD)
-            print("Superuser created!")
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(username, email, password)
+            print("Superuser created successfully!")
+        else:
+            print("Superuser already exists!")
