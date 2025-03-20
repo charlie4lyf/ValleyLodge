51a2091e342d2ab69145257b2e208681ad4eee49
charlie4lyf <monzoracharles55@gmail.com>
2025-03-20 16:36:54 +0200

    11th

--- website/settings.py
+++ website/settings.py
@@ -134,9 +134,9 @@ LOGIN_URL = 'login'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 ALLOWED_HOSTS = ['valleylodge.onrender.com']
-
-AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
-AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME')
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_STORAGE_BUCKET_NAME = os.getenv('valleylodge-media-bucket')
+AWS_S3_REGION_NAME = os.getenv('us-west-2')
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_DEFAULT_ACL = None
@@ -146,6 +146,6 @@ AWS_S3_OBJECT_PARAMETERS = {
 }
 
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com'
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
 
